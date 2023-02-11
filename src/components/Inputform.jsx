1aa822d666c5b769ee4f97c5a290d17fa9091dc7
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2023-02-11 19:21:14 +0900

    todoの追加機能の実装

---
+++ src/components/Inputform.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+export const Inputform = ({taskList, setTaskList}) => {
+
+    const [inputText, setInputText] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTaskList([
+            ...taskList,
+            {
+                text: inputText,
+                state: '作業中'
+            }
+        ])
+    }
+    const handleChange = (e) => {
+        setInputText(e.target.value);
+    }
+
+    return (
+        <div className='input-form'>
+            <h2 className='title'>新規タスクの追加</h2>
+            <form className='form' onSubmit={ handleSubmit }>
+                <input className='input' type="text" onChange={ handleChange }/>
+                <button className='submit'>追加</button>
+            </form>
+        </div>
+    )
+}
