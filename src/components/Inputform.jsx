76a8f034c20a6f4a946e0c6bb257beabd1fe0b2b
kakky08 <77216691+kakky08@users.noreply.github.com>
2023-02-13 22:18:37 +0900

    Update Inputform.jsx

--- src/components/Inputform.jsx
+++ src/components/Inputform.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 export const Inputform = ({taskList, setTaskList}) => {
 
-    const [inputText, setInputText] = useState("");
+    const [inputText, setInputText] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
