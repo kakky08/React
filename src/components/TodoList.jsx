0887cfeee57e57f8aefe430fa5a18e15a530cb41
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2023-02-19 16:51:51 +0900

    タスクの表示/非表示機能の実装

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
+import { useState } from 'react';
 
 export const TodoList = ({ taskList, setTaskList }) => {
+
+    const [state, setState] = useState('すべて');
 
     const handleDelete = (id) => {
         setTaskList(taskList.filter((task) => task.id !== id));
     }
 
-    const handleChengeState = (index) => {
-        taskList[index].state = taskList[index].state === '作業中' ? '完了' : '作業中';
+    const handleChengeState = (task) => {
+        task.state = task.state === '作業中' ? '完了' : '作業中';
         setTaskList([...taskList]);
+    }
+
+    const handleStateSearch = (e) => {
+        setState(e.target.value);
     }
 
     return (
         <div className='todo-list'>
             <form className='form'>
-                <input className='input' type="radio" name="state" value="all" />すべて
-                <input className='input' type="radio" name="state" value="work" />作業中
-                <input className='input' type="radio" name="state" value="completion" />完了
+                <input className='input' type="radio" name="state" value="すべて" onClick={handleStateSearch} defaultChecked/>すべて
+                <input className='input' type="radio" name="state" value="作業中" onClick={handleStateSearch}/>作業中
+                <input className='input' type="radio" name="state" value="完了" onClick={handleStateSearch}/>完了
             </form>
             <table className='table'>
                 <thead className='thead'>
@@ -27,12 +34,12 @@ export const TodoList = ({ taskList, setTaskList }) => {
                     </tr>
                 </thead>
                 <tbody className='tbody'>
-                    {taskList.map((task, index) => (
-                        <tr className='row' key={index}>
+                    {taskList.filter(task => state === task.state || state === 'すべて').map((task, index) => (
+                        <tr  className='row' key={index}>
                             <td className='data'>{ task.id }</td>
                             <td className='data'>{ task.text }</td>
                             <td className='data'>
-                                <button className='button completeion' onClick={() => handleChengeState(index)}>{ task.state }</button>
+                                <button className='button completeion' onClick={() => handleChengeState(task)}>{ task.state }</button>
                                 <button className='button delete' onClick={() => handleDelete(task.id)}>削除</button>
                             </td>
                         </tr>
